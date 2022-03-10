41ed6eaaeb0ea37f5ffdda41e380e1cef721af52
Maciej Dobrzynski <macdobry@gmail.com>
2022-03-10 12:28:23 +0100

    Added the explicit colour palette

--- R/plotNoodle2D.R
+++ R/plotNoodle2D.R
@@ -3,11 +3,12 @@
 #' Plot tracks in 2D.
 #'
 #' @title "Plot collective events"
-#' @param obj an arcosTS object.
+#' @param objTS an arcosTS object.
 #' @param xfac a numeric, multiplication factor for the x-axis; default 1.
 #' @param yfac a numeric, multiplication factor for the y-axis; default 1.
 #' @param tfreq a numeric, frequency of the temporal column; default 1.
 #' @param pos an integer, index of the coordinate to plot in the position vector; default 1L.
+#' @param colpal a vector with colours in hex format, default colours from the Tableau 20 palette.
 #'
 #' @return a ggplot2 object.
 #'
@@ -19,14 +20,22 @@
 plotNoodle2D <- function(objTS,
                          xfac = 1, yfac = 1,
                          tfreq = 1,
-                         pos = 1L) {
+                         pos = 1L,
+                         colpal = c("#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F",
+                                    "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
+                                    "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295",
+                                    "#FABFD2", "#B07AA1", "#D4A6C8", "#9D7660", "#D7B5A6")) {
   UseMethod("plotNoodle2D")
 }
 
 plotNoodle2D.default <- function(objTS,
                                  xfac = 1, yfac = 1,
                                  tfreq = 1,
-                                 pos = 1L) {
+                                 pos = 1L,
+                                 colpal = c("#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F",
+                                            "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
+                                            "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295",
+                                            "#FABFD2", "#B07AA1", "#D4A6C8", "#9D7660", "#D7B5A6")) {
   cat("This is a generic function\n")
 }
 
@@ -36,7 +45,11 @@ plotNoodle2D.default <- function(objTS,
 plotNoodle2D.arcosTS = function(objTS,
                                 xfac = 1, yfac = 1,
                                 tfreq = 1,
-                                pos = 1L) {
+                                pos = 1L,
+                                colpal = c("#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F",
+                                           "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
+                                           "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295",
+                                           "#FABFD2", "#B07AA1", "#D4A6C8", "#9D7660", "#D7B5A6")) {
 
   stopifnot(is.arcosTS(objTS))
 
@@ -86,8 +99,8 @@ plotNoodle2D.arcosTS = function(objTS,
                   group = pathgrp,
                   color = as.factor(collgrp))) +
     geom_path(size = 0.25, na.rm = T) +
-    scale_color_manual(values = rep(rep(ggthemes::tableau_color_pal(palette = "Tableau 20")(20),
-                                        ceiling(max(objTS[["collgrp"]]) / 20))))
+    scale_color_manual(values = rep(rep(colpal,
+                                        ceiling(max(objTS[["collgrp"]]) / length(colpal)))))
 
   return(p1)
 }
