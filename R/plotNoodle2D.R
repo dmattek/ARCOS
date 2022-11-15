40fc622e736a1ad08ae616e9f1c5e8ab600b35f4
Maciej Dobrzynski <macdobry@gmail.com>
2022-11-15 16:02:29 +0100

    Added plotting points lines or both

--- R/plotNoodle2D.R
+++ R/plotNoodle2D.R
@@ -1,6 +1,6 @@
-#' Plot tracks from an arcosTS object
+#' Noodle/spaghetti plot
 #'
-#' Plot tracks in 2D.
+#' Visualise active objects/cells when they participate in collective events. The plot projects one of the spatial dimensions and time on a 2D space.
 #'
 #' @title "Plot collective events"
 #' @param objTS an arcosTS object.
@@ -8,6 +8,9 @@
 #' @param yfac a numeric, multiplication factor for the y-axis; default 1.
 #' @param tfreq a numeric, frequency of the temporal column; default 1.
 #' @param pos an integer, index of the coordinate to plot in the position vector; default 1L.
+#' @param style one of 'line', 'point', 'both'; determines how collective events are plotted; as lines, points or both.
+#' Note that lines can only visualise tracks that span at least 2 frames. Choose 'points' or 'both' for events that consist of many 1-frame tracks.
+#' @param size a numeric, determines thickness and size of lines and plots; default 0.25.
 #' @param colpal a vector with colours in hex format, default colours from the Tableau 20 palette.
 #'
 #' @return a ggplot2 object.
@@ -21,6 +24,8 @@ plotNoodle2D <- function(objTS,
                          xfac = 1, yfac = 1,
                          tfreq = 1,
                          pos = 1L,
+                         style = c('line', 'point', 'both'),
+                         size = 0.25,
                          colpal = c("#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F",
                                     "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
                                     "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295",
@@ -32,6 +37,8 @@ plotNoodle2D.default <- function(objTS,
                                  xfac = 1, yfac = 1,
                                  tfreq = 1,
                                  pos = 1L,
+                                 style = c('line', 'point', 'both'),
+                                 size = 0.25,
                                  colpal = c("#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F",
                                             "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
                                             "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295",
@@ -46,6 +53,8 @@ plotNoodle2D.arcosTS = function(objTS,
                                 xfac = 1, yfac = 1,
                                 tfreq = 1,
                                 pos = 1L,
+                                style = c('line', 'point', 'both'),
+                                size = 0.25,
                                 colpal = c("#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F",
                                            "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
                                            "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295",
@@ -57,6 +66,8 @@ plotNoodle2D.arcosTS = function(objTS,
   colIDobj <- attr(objTS, "colIDobj")
   colIDcoll <- attr(objTS, "colIDcoll")
   colPos <- attr(objTS, "colPos")
+
+  style <- match.arg(style)
 
   # add a grouping column;
   # grouping runs per collective event and per trackid
@@ -98,9 +109,11 @@ plotNoodle2D.arcosTS = function(objTS,
                   y = get(colPos[pos]) * yfac,
                   group = pathgrp,
                   color = as.factor(collgrp))) +
-    geom_path(size = 0.25, na.rm = T) +
     scale_color_manual(values = rep(rep(colpal,
                                         ceiling(max(objTS[["collgrp"]]) / length(colpal)))))
+
+  if (style %in% c('line', 'both')) p1 = p1 + geom_path(size = size, na.rm = T)
+  if (style %in% c('point', 'both')) p1 = p1 + geom_point(size = size, na.rm = T)
 
   return(p1)
 }
