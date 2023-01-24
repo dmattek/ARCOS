6b8c9352a8d2fd2ec3a8da20406f1ed760259faa
Maciej Dobrzynski <macdobry@gmail.com>
2023-01-24 11:20:38 +0100

    Calculate only one-sided p-val.

--- R/calcPvalFromBS.R
+++ R/calcPvalFromBS.R
@@ -1,11 +1,11 @@
 #' Calculate p-value from bootstrapping
 #'
-#' The corrected p-value calculate uses the formula from Davison and Hinkley (1997), Bootstrap Methods and their Application, p. 141.
+#' The calculation of the p-value that accounts for the finite sample size uses a formula from Davison and Hinkley (1997), Bootstrap Methods and their Application, chapter 4, p. 161.
 #'
 #' @param testStat a numeric, test statistic.
 #' @param bsStat a numeric vector with statistic from bootstrapping.
-#' @param corrected a logical; if TRUE, calculates a corrected p-value; default FALSE.
-#' @param alternative either of "two.sided", "less", "greater".
+#' @param corrected a logical; if TRUE, calculates a p-value that accounts for the finite sample size; default FALSE.
+#' @param alternative either of "less" or "greater"; default "greater"
 #'
 #' @return a scalar with the p-value
 #'
@@ -15,7 +15,7 @@
 #' @examples
 #' library(ARCOS)
 #' ARCOS::calcPvalFromBS(2, rnorm(1000))
-calcPvalFromBS <- function(testStat, bsStat, corrected = FALSE, alternative = c("two.sided", "less", "greater")) {
+calcPvalFromBS <- function(testStat, bsStat, corrected = FALSE, alternative = c("greater", "less")) {
 
   stopifnot(length(testStat) == 1)
   stopifnot(is.vector(bsStat))
@@ -25,20 +25,14 @@ calcPvalFromBS <- function(testStat, bsStat, corrected = FALSE, alternative = c(
   if (locN <= 20) warning('The size of the bootstrap should be greater than 20 to calculate the significance level 0.05')
 
   if (corrected) {
-    if (alternative == "two.sided") {
-      locPval = 2 * min((1 + sum(bsStat <= testStat, na.rm = T)) / (locN + 1),
-                        (1 + sum(bsStat >= testStat, na.rm = T))  / (locN + 1))
-    } else if (alternative == "less") {
+    if (alternative == "less") {
       locPval = (1 + sum(bsStat <= testStat, na.rm = T)) / (locN + 1)
     } else {
       locPval = (1 + sum(bsStat >= testStat, na.rm = T)) / (locN + 1)
     }
 
   } else {
-    if (alternative == "two.sided") {
-      locPval = 2 * min(mean(bsStat <= testStat, na.rm = T),
-                        mean(bsStat >= testStat, na.rm = T))
-    } else if (alternative == "less") {
+    if (alternative == "less") {
       locPval = mean(bsStat <= testStat, na.rm = T)
     } else {
       locPval = mean(bsStat >= testStat, na.rm = T)
